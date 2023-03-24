86b665fe9c8adf79af31266891ab97428dc6fb3b
max.isi <max.isi@ligo.org>
2023-03-23 20:43:40 -0400

    extra panel in Fig 4

--- src/scripts/control_isotropized_plot.py
+++ src/scripts/control_isotropized_plot.py
@@ -42,7 +42,7 @@ RNG = np.random.default_rng(12345)
 
 # load fits in order of number of vectors in simulated catalog
 fit_path = str(paths.data / "control_isotropized/fit_{}.nc")
-nvecs = sorted([int(parse(fit_path, p)[0]) for p in glob(fit_path.format('*'))])
+nvecs = sorted([int(parse(fit_path,p)[0]) for p in glob(fit_path.format('*'))])
 incr_fits = [az.from_netcdf(fit_path.format(n)) for n in nvecs]
 
 nsamp = 4000
@@ -61,11 +61,18 @@ for i, fake_fit in enumerate(incr_fits):
 ###############################################################################
 
 #with sns.axes_style("whitegrid", {"grid.linestyle": ':'}):
+cmap = 'crest_r'
 kde_kws = dict(common_norm=False, alpha=0.7)
-g = sns.PairGrid(df, hue='N', diag_sharey=False, corner=True,
-             palette='crest_r')
+g = sns.PairGrid(df, hue='N', diag_sharey=False, corner=True, palette=cmap)
 g.map_lower(sns.kdeplot, levels=1, thresh=0.1, **kde_kws)
 g.map_diag(sns.kdeplot, **kde_kws)
+
+ax = g.axes[0,0]
+cs = sns.color_palette(cmap, n_colors=len(nvecs))
+for n, c in zip(nvecs, cs):
+    ax.plot([], [], label="$N = {}$".format(n), c=c)
+ax.legend(loc='center left', bbox_to_anchor=(1.25, 0.5), frameon=False,
+          fontsize=14)
 
 for i, axs in enumerate(g.axes):
     for j, ax in enumerate(axs):
@@ -77,6 +84,19 @@ for i, axs in enumerate(g.axes):
                 ax.set_ylim(-1, 1)
                 if j == 0:
                     ax.set_yticks([-1, 0, 1])
+
+ax0 = g.axes[0,0]
+ax1 = g.axes[-1,-1]
+ax = g.fig.add_axes([ax1.get_position().x0, ax0.get_position().y0,
+                     ax0.get_position().width, ax0.get_position().height])
+df['norm'] = np.linalg.norm([df[k] for k in keys], axis=0)
+df_norm = df[['norm', 'N']]
+sns.kdeplot(data=df_norm, x='norm', hue='N', ax=ax, palette=cmap, common_norm=False)
+ax.get_legend().remove()
+ax.set_xlabel(r"$|\vec{v}_J|$")
+ax.set_ylabel('')
+ax.get_yaxis().set_visible(False)
+ax.set_xlim(0, 1)
 
 fname = paths.figures / "control_isotropized.pdf"
 g.fig.savefig(fname, bbox_inches="tight")
